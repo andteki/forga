140c2da298debe0ebd1e937bdf9e730b9b912cbd
Sallai András <andteki@gmail.com>
2021-11-22 09:55:05 +0100

    Néhány komponens kész

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  TouchableHighlight,
+  Button 
+} from 'react-native';
 
 export default function App() {
+  const [all, setAll] = useState(0);
+  const [car, setCar] = useState(0);
+
+  function handleDelButton() {
+    setAll(0);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Forgalom számláló</Text>
+      
+      <TouchableHighlight
+          style={styles.runButton}
+          onPress={handleDelButton}
+          >
+          <Text style={styles.runText}>Mindent töröl</Text>
+      </TouchableHighlight>
+
+      <Text>Összes: {all}</Text>
+      <Text>{car}</Text>
+
+      <TouchableHighlight
+          style={styles.runButton}
+          onPress={handleDelButton}
+          >
+          <Text style={styles.runText}>Mindent töröl</Text>
+      </TouchableHighlight>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +48,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  runButton: {
+    backgroundColor: 'blue',
+    marginTop: 10,
+    marginBottom: 70,
+    padding: 10,
+    borderRadius: 3,
+
+  },
+  runText: {
+    color: 'white',
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontSize: 22,
+  },  
 });
